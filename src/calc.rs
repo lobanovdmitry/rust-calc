bb74e44a8b980b51d4a2995958809f26900c3452
lobanov.dmitr <lobanov.dmitr@gmail.com>
2022-10-14 23:12:14 +0300

    refactoring

--- src/calc.rs
+++ src/calc.rs
@@ -15,12 +15,14 @@ impl Calc for SimpleCalc {
 
 pub struct CalcConf {
     operations: HashMap<&'static str, u8>, // operation with priorities
+    number_regex: Regex,
 }
 
 impl CalcConf {
     pub fn new() -> Self {
         Self {
             operations: HashMap::from([("+", 0), ("-", 0), ("*", 1), ("/", 1), ("^", 2)]),
+            number_regex: Regex::new("^(-?[1-9]\\d*|-?0)(\\.\\d*)?").unwrap(),
         }
     }
 }
@@ -55,113 +57,35 @@ impl CalcErr {
 type CalcStack = Vec<CalcExprItem>;
 
 impl SimpleCalc {
-    /// Converts input string into vection of expression in reverse polish notation.
+    /// Converts input string into vection of expression in Reverse Polish Notation.
     fn convert_to_rpn(&self, input: &str) -> Result<CalcStack, CalcErr> {
         if !input.is_ascii() {
             return Err(CalcErr::new("Input must contain only ascii characters!"));
         }
-        let mut result: CalcStack = vec![];
         let mut stack: CalcStack = vec![];
-        let number_regex = Regex::new("^(-?[1-9]\\d*|-?0)(\\.\\d*)?").unwrap();
+        let mut result: CalcStack = vec![];
         let mut i = 0;
         while i < input.len() {
-            let first_char = &input[i..i + 1];
-            if first_char == " " || first_char == "\t" {
-                // ignoring whitespaces and tabs
-                i += 1;
-                continue;
-            }
-            if first_char == "(" {
-                stack.push(CalcExprItem::OpenBracket);
-                i += 1;
-                continue;
-            }
-            if first_char == ")" {
-                loop {
-                    match stack.pop() {
-                        Some(CalcExprItem::OpenBracket) => break,
-                        Some(x) => result.push(x),
-                        None => return Err(CalcErr::new("brackets are not alignt in the expression!!!")),
-                    }
-                }
-                i += 1;
+            let next_char = &input[i..i + 1];
+            let len = self.parse_prefix(next_char, &mut stack, &mut result)?;
+            if len > 0 {
+                i += len;
                 continue;
             }
             let prefix = &input[i..];
-            // processing of number
-            match number_regex.find(prefix) {
-                Some(m) if m.start() == 0 => {
-                    let num_str = &prefix[0..m.end()];
-                    let number = num_str
-                        .parse::<f64>()
-                        .map_err(|e| CalcErr::new(&format!("failed to parse number: {} err={}", m.as_str(), e)))?;
-                    result.push(CalcExprItem::Number(number));
-                    i += num_str.len();
-                    continue;
-                }
-                _ => {}
-            }
-
-            // processing binary infix functions
-            if ["+", "-", "*", "/", "^"].contains(&first_char) {
-                loop {
-                    let need_push;
-                    if let Some(top) = stack.last() {
-                        match top {
-                            CalcExprItem::UniOp(_, _) => {
-                                need_push = true;
-                            }
-                            CalcExprItem::BinOp(x, _) => {
-                                if self.conf.operations.get(x) >= self.conf.operations.get(&first_char) {
-                                    need_push = true;
-                                } else {
-                                    break;
-                                }
-                            }
-                            _ => break,
-                        }
-                    } else {
-                        break;
-                    }
-                    if need_push {
-                        let x = stack.pop().unwrap();
-                        result.push(x);
-                    }
-                }
-                match first_char {
-                    "+" => stack.push(CalcExprItem::BinOp("+", Box::new(|x, y| x + y))),
-                    "-" => stack.push(CalcExprItem::BinOp("-", Box::new(|x, y| x - y))),
-                    "*" => stack.push(CalcExprItem::BinOp("*", Box::new(|x, y| x * y))),
-                    "/" => stack.push(CalcExprItem::BinOp("/", Box::new(|x, y| x / y))),
-                    "^" => stack.push(CalcExprItem::BinOp("^", Box::new(|x, y| x.powf(y)))),
-                    _ => panic!("why we here then???"),
-                }
-                i += 1;
+            let len = self.parse_number(prefix, &mut result)?;
+            if len > 0 {
+                i += len;
                 continue;
             }
-            // processing unary prefix functions
-            if prefix.starts_with("sin") {
-                stack.push(CalcExprItem::UniOp(
-                    "sin",
-                    Box::new(|x| f64::sin(x * std::f64::consts::PI / 180.0)),
-                ));
-                i += 3;
+            let len = self.parse_binary_op(next_char, &mut stack, &mut result)?;
+            if len > 0 {
+                i += len;
                 continue;
             }
-            if prefix.starts_with("cos") {
-                stack.push(CalcExprItem::UniOp(
-                    "cos",
-                    Box::new(|x| f64::cos(x * std::f64::consts::PI / 180.0)),
-                ));
-                i += 3;
-                continue;
-            }
-            if prefix.starts_with("tan") {
-                stack.push(CalcExprItem::UniOp(
-                    "tan",
-                    Box::new(|x| f64::tan(x * std::f64::consts::PI / 180.0)),
-                ));
-                i += 3;
+            let len = self.parse_unary_op(prefix, &mut stack)?;
+            if len > 0 {
+                i += len;
                 continue;
             }
             return Err(CalcErr::new(&format!("Can't process {i}")));
@@ -175,33 +99,135 @@ impl SimpleCalc {
         Ok(result)
     }
 
-    fn calc_from_rpn(&self, expr_items: Vec<CalcExprItem>) -> Result<f64, CalcErr> {
+    fn parse_prefix(&self, next_char: &str, stack: &mut CalcStack, result: &mut CalcStack) -> Result<usize, CalcErr> {
+        match next_char {
+            " " | "\t" => Ok(1),
+            "(" => {
+                stack.push(CalcExprItem::OpenBracket);
+                Ok(1)
+            }
+            ")" => {
+                loop {
+                    match stack.pop() {
+                        Some(CalcExprItem::OpenBracket) => break,
+                        Some(x) => result.push(x),
+                        None => return Err(CalcErr::new("brackets are not alignt in the expression!!!")),
+                    }
+                }
+                Ok(1)
+            }
+            _ => Ok(0),
+        }
+    }
+
+    fn parse_number(&self, input: &str, result: &mut CalcStack) -> Result<usize, CalcErr> {
+        if let Some(m) = self.conf.number_regex.find(input) {
+            let num_str = &input[0..m.end()];
+            let number = num_str
+                .parse::<f64>()
+                .map_err(|e| CalcErr::new(&format!("Failed to parse number: {} err={}", m.as_str(), e)))?;
+            result.push(CalcExprItem::Number(number));
+            Ok(num_str.len())
+        } else {
+            Ok(0)
+        }
+    }
+
+    fn parse_binary_op(
+        &self,
+        next_char: &str,
+        stack: &mut CalcStack,
+        result: &mut CalcStack,
+    ) -> Result<usize, CalcErr> {
+        if ["+", "-", "*", "/", "^"].contains(&next_char) {
+            loop {
+                if let Some(top) = stack.last() {
+                    match top {
+                        CalcExprItem::UniOp(_, _) => {
+                            stack.pop().map(|x| result.push(x));
+                        }
+                        CalcExprItem::BinOp(x, _) => {
+                            let top_op_priority = self.conf.operations.get(x);
+                            let cur_op_priority = self.conf.operations.get(&next_char);
+                            if top_op_priority >= cur_op_priority {
+                                stack.pop().map(|x| result.push(x));
+                            } else {
+                                break;
+                            }
+                        }
+                        _ => break,
+                    }
+                } else {
+                    break;
+                }
+            }
+            match next_char {
+                "+" => stack.push(CalcExprItem::BinOp("+", Box::new(|x, y| x + y))),
+                "-" => stack.push(CalcExprItem::BinOp("-", Box::new(|x, y| x - y))),
+                "*" => stack.push(CalcExprItem::BinOp("*", Box::new(|x, y| x * y))),
+                "/" => stack.push(CalcExprItem::BinOp("/", Box::new(|x, y| x / y))),
+                "^" => stack.push(CalcExprItem::BinOp("^", Box::new(|x, y| x.powf(y)))),
+                _ => panic!("why we here then???"),
+            }
+            Ok(1)
+        } else {
+            Ok(0)
+        }
+    }
+
+    fn parse_unary_op(&self, input: &str, stack: &mut CalcStack) -> Result<usize, CalcErr> {
+        const SIN: &str = "sin";
+        const COS: &str = "cos";
+        const TAN: &str = "tan";
+        let (item, len) = if input.starts_with(SIN) {
+            let sin = |x: f64| SimpleCalc::rad2degree(x).sin();
+            let sin_op = CalcExprItem::UniOp(SIN, Box::new(sin));
+            (sin_op, 3)
+        } else if input.starts_with(COS) {
+            let cos = |x: f64| SimpleCalc::rad2degree(x).cos();
+            let cos_op = CalcExprItem::UniOp(COS, Box::new(cos));
+            (cos_op, 3)
+        } else if input.starts_with(TAN) {
+            let tan = |x: f64| SimpleCalc::rad2degree(x).tan();
+            let tan_op = CalcExprItem::UniOp(TAN, Box::new(tan));
+            (tan_op, 3)
+        } else {
+            return Ok(0);
+        };
+        stack.push(item);
+        Ok(len)
+    }
+
+    fn rad2degree(x: f64) -> f64 {
+        x * std::f64::consts::PI / 180.0
+    }
+
+    /// Calculates final number based on Reverse Polish Notation.
+    fn calc_from_rpn(&self, rpn_vec: CalcStack) -> Result<f64, CalcErr> {
+        assert!(!rpn_vec.is_empty(), "rpc_vec must be non empty!");
         let mut stack = Vec::<f64>::new();
-        for item in expr_items {
+        for item in rpn_vec {
             match item {
                 CalcExprItem::UniOp(_, op) => {
                     let arg = stack.pop().ok_or(CalcErr::new("No arg for unary operation."))?;
                     stack.push(op(arg));
                 }
                 CalcExprItem::BinOp(_, op) => {
-                    let arg_2 = stack
-                        .pop()
-                        .ok_or(CalcErr::new("Illegal state: binary operation args invalid."))?;
-                    let arg_1 = stack
-                        .pop()
-                        .ok_or(CalcErr::new("Illegal state: binary operation args invalid."))?;
+                    let arg_2 = stack.pop().ok_or(CalcErr::new("Binary operation args invalid."))?;
+                    let arg_1 = stack.pop().ok_or(CalcErr::new("Binary operation args invalid."))?;
                     stack.push(op(arg_1, arg_2));
                 }
-                CalcExprItem::Number(value) => stack.push(value),
-                _ => return Err(CalcErr::new("wtf?")),
+                CalcExprItem::Number(value) => {
+                    stack.push(value);
+                }
+                _ => {
+                    panic!("RPN must contain only unary or binary operation or number.");
+                }
             }
         }
-        let result = stack.pop().ok_or(CalcErr::new("Illegal state: stack is empty."))?;
+        let result = stack.pop().ok_or(CalcErr::new("Stack is empty."))?;
         if !stack.is_empty() {
-            return Err(CalcErr::new(&format!(
-                "Illegal state: stack is not empty after compute. remainings: {:?}",
-                stack
-            )));
+            panic!("Stack is not empty after calc. Remainings: {:?}", stack);
         }
         Ok(result)
     }
